47f7b6fb663a665e9376535b630d90ac0c857ba5
Tehreem-Fatima <tehreem.lc@outlook.com>
2022-08-16 12:18:58 +0500

    first commit

---
+++ include/config.php
@@ -0,0 +1,18 @@
+<?php 
+
+//Define your host here.
+$HostName = "localhost";
+ 
+//Define your database username here.
+$HostUser = "root";
+ 
+//Define your database password here.
+$HostPass = "root";
+
+//Define your database name here.
+$DatabaseName = "myportfolio";
+
+$conn = mysqli_connect("$HostName", "$HostUser", "$HostPass", "$DatabaseName");
+
+
+ ?>
